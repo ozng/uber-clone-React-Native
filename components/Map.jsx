d8b2ee4fe7a304bbc89e06799f56be031c3c1f39
ozng <ozangurer12@gmail.com>
2022-08-25 16:10:03 +0300

    Map com created

---
+++ components/Map.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const Map = () => {
+  return (
+    <View>
+      <Text>This is Map Component</Text>
+    </View>
+  );
+};
+
+export default Map;
+
+const styles = StyleSheet.create({});
