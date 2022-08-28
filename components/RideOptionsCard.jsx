6e2fb87b83c052781b9b123f9e484f1546f155db
ozng <ozangurer12@gmail.com>
2022-08-28 12:14:36 +0300

    RideOptionCard UI

--- components/RideOptionsCard.jsx
+++ components/RideOptionsCard.jsx
@@ -1,10 +1,84 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { Icon } from "@rneui/base";
+import { useState } from "react";
+import {
+  FlatList,
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import tw from "tailwind-react-native-classnames";
 
-const RideOptionsCard = () => {
+const data = [
+  {
+    id: "Uber-X-123",
+    title: "UberX",
+    multiplier: 1,
+    image: "https://links.papareact.com/3pn",
+  },
+  {
+    id: "Uber-X-456",
+    title: "Uber XL",
+    multiplier: 1.2,
+    image: "https://links.papareact.com/5w8",
+  },
+  {
+    id: "Uber-X-789",
+    title: "Uber LUX",
+    multiplier: 1.76,
+    image: "https://links.papareact.com/7pf",
+  },
+];
+
+const RideOptionsCard = ({ navigation }) => {
+  const [selected, setSelected] = useState(null);
+
   return (
-    <View>
-      <Text>RideOptionsCard</Text>
+    <View style={tw`bg-white flex-grow`}>
+      <View>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("NavigateCard")}
+          style={tw`absolute top-3 left-5 p-3 z-50 rounded-full`}
+        >
+          <Icon name="chevron-left" type="fontawesome" />
+        </TouchableOpacity>
+        <Text style={tw`text-center py-5 text-xl`}>Select a Ride</Text>
+      </View>
+
+      <FlatList
+        data={data}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            activeOpacity={0.65}
+            onPress={() => setSelected(item)}
+            style={tw`flex-row justify-between items-center px-10 ${
+              item.id === selected?.id && "bg-gray-200"
+            }`}
+          >
+            <Image
+              style={{ width: 70, height: 70, resizeMode: "contain" }}
+              source={{ uri: item.image }}
+            />
+            <View style={tw`-ml-6`}>
+              <Text style={tw`text-xl font-semibold`}>{item.title}</Text>
+              <Text>Travel time.</Text>
+            </View>
+            <Text style={tw`text-xl`}>99₺</Text>
+          </TouchableOpacity>
+        )}
+      />
+      <View>
+        <TouchableOpacity
+          activeOpacity={0.65}
+          style={tw`bg-black py-3 m-3 ${!selected && "bg-gray-300"}`}
+          disabled={!selected}
+        >
+          <Text style={tw`text-center text-white text-xl`}>
+            Choose {selected?.title}
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
